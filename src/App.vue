6170d7addb3ff53b34503da4405c3f7dfdac2835
Nari Jeong <narijeong@icloud.com>
2023-02-22 17:05:03 +0900

    add diaglo

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,98 @@
 <template>
+  <div>
   <v-app id="inspire">
     <v-app-bar
       app
+      color="primary"
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
 
       <v-toolbar-title>QR Code</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn icon>
+      <!-- <v-btn icon>
         <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-app-bar>
 
     <v-main>
-        <!-- <v-row>
-          <v-col
-            v-for="n in 24"
-            :key="n"
-            cols="4"
-          >
-            <v-card height="200"></v-card>
-          </v-col>
-        </v-row> -->
-        <div id="reader" class="ml-0"></div>
+      <div id="reader"></div>
     </v-main>
   </v-app>
+  <v-row justify="center">
+  <v-dialog
+      v-model="dialog"
+      fullscreen
+      :scrim="false"
+      transition="dialog-bottom-transition"
+    >
+      <!-- <template v-slot:activator="{ props }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="props"
+        >
+          Open Dialog
+        </v-btn>
+      </template> -->
+      <v-card>
+        <v-toolbar
+          dark
+          color="primary"
+        >
+          <v-btn
+            icon
+            dark
+            @click="dialog = false"
+          >
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          <v-toolbar-title>Settings</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn
+              variant="text"
+              @click="dialog = false"
+            >
+              Save
+            </v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+        <v-list
+          lines="two"
+          subheader
+        >
+          <v-list-subheader>User Controls</v-list-subheader>
+          <v-list-item title="Content filtering" subtitle="Set the content filtering level to restrict apps that can be downloaded"></v-list-item>
+          <v-list-item title="Password" subtitle="Require password for purchase or use password to restrict purchase"></v-list-item>
+        </v-list>
+        <v-divider></v-divider>
+        <v-list
+          lines="two"
+          subheader
+        >
+          <v-list-subheader>General</v-list-subheader>
+          <v-list-item title="Notifications" subtitle="Notify me about updates to apps or games that I downloaded">
+            <template v-slot:prepend>
+              <v-checkbox v-model="notifications"></v-checkbox>
+            </template>
+          </v-list-item>
+          <v-list-item title="Sound" subtitle="Auto-update apps at any time. Data charges may apply">
+            <template v-slot:prepend>
+              <v-checkbox v-model="sound"></v-checkbox>
+            </template>
+          </v-list-item>
+          <v-list-item title="Auto-add widgets" subtitle="Automatically add home screen widgets">
+            <template v-slot:prepend>
+              <v-checkbox v-model="widgets"></v-checkbox>
+            </template>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-dialog>
+  </v-row>
+  </div>
 </template>
 
 
@@ -42,7 +108,12 @@ export default {
         return {
             // value: "https://example.com",
             // size: 300,
+            dialog: false,
+            notifications: false,
+            sound: true,
+            widgets: false,
             url: String,
+            ips: [{id:1, desc: 'ip 1 description'}, {id:1, desc: 'ip 1 description'}]
         };
     },
   mounted() {
@@ -84,11 +155,12 @@ export default {
         console.log(decodedText)
         console.log(decodedResult)
         this.url = decodedText
-        html5QrCode.stop().then((ignore) => {
-          // QR Code scanning is stopped.
-          }).catch((err) => {
-            // Stop failed, handle it.
-          });
+        this.dialog = true
+        // html5QrCode.stop().then((ignore) => {
+        //   // QR Code scanning is stopped.
+        //   }).catch((err) => {
+        //     // Stop failed, handle it.
+        //   });
       },
       (errorMessage) => {
         // parse error, ignore it.
@@ -104,3 +176,10 @@ export default {
   }
 }
 </script>
+
+<style>
+.dialog-bottom-transition-enter-active,
+.dialog-bottom-transition-leave-active {
+  transition: transform .2s ease-in-out;
+}
+</style>
